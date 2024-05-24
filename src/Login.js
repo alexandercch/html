2cad1d771afbdb59116a6465116464d5f8de9f43
alex cc <alexander.cc.h@gmail.com>
2024-05-23 23:28:23 -0500

    Login btn

--- src/Login.js
+++ src/Login.js
@@ -1,14 +1,22 @@
-import { createElement } from './utils';
+import { createElement } from "./utils";
 
 function Login() {
-  const title = createElement('h2', { textContent: 'Login Page' });
+  const title = createElement("h2", { textContent: "Login Page" });
 
-  const dashboardLink = createElement('a', {
-    href: '/#/dashboard',
-    textContent: 'go to dashboard',
+  const dashboardLink = createElement("a", {
+    href: "/#/dashboard",
+    textContent: "go to dashboard",
   });
 
-  return createElement('div', {}, [title, dashboardLink]);
+  const loginButton = createElement("button", {
+    id: "loginButton",
+    textContent: "Login",
+  });
+  loginButton.addEventListener("click", ()=> {
+    //  verify user exist in mock data
+    window.location.hash = '#/dashboard';
+  })
+  return createElement("div", {}, [title, dashboardLink, loginButton]);
 }
 
 export default Login;
